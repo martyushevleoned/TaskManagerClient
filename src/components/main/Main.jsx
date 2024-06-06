6b1aec41dc2e840ee3a5c6ae638f79518b075c79
Martyushev Leoned <Lyonya.martyushev@mail.ru>
2024-06-06 22:25:52 +0500

    main page

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,26 @@
-import React, {useState} from 'react';
+//import React, {useState} from 'react';
 import './Main.css'
 
 function Main({getToken, removeToken}) {
   return(
     <div className='body-main'>
-      <div>
-        <p>Left</p>
-        <button onClick={removeToken}>Logout</button>
-      </div>
-      <div>
-        <p>Right</p>
+      <div className='wrap'>
+        <div className='flex user-bar'>
+          <div className='border'>
+            <div><span>Username: </span></div>
+            <div><span>Email: </span></div>
+            <button onClick={removeToken}>Logout</button>
+          </div>
+          <div className='border tag'>
+            <div><span>Tag info: </span></div>
+          </div>
+        </div>
+        <div className='flex'>
+          <div className='border tasks'>
+            <div><span>task1</span></div>
+            <div><span>task2</span></div>
+          </div>
+        </div>
       </div>
     </div>
   );
