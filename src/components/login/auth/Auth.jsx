4bb87a9aad4816f152a205ae99ffcbb977a8ee07
Martyushev Leoned <Lyonya.martyushev@mail.ru>
2024-06-06 13:00:53 +0500

    jwt mvp

--- src/components/login/auth/Auth.jsx
+++ src/components/login/auth/Auth.jsx
@@ -3,19 +3,48 @@ import {FaUser, FaLock} from "react-icons/fa";
 
 const Auth = ({setRegisterPage}) => {
 
-    const handle = (event) => setRegisterPage();
+    const handle = () => setRegisterPage();
+
+    const delay = (delayInms) => {return new Promise(resolve => setTimeout(resolve, delayInms));};
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+
+        console.log(event.target.username.value);
+        console.log(event.target.password.value);
+
+        fetch("http://localhost:8080/login", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify({
+                "username": event.target.username.value,
+                "password": event.target.password.value
+            })})
+            .then(response => {
+                if (response.ok) {
+                    alert("Аутентефикация прошла успешно");
+                    delay(3000);
+                    response.text().then(token => {
+                        console.log(token);
+                        localStorage.setItem('jwt', token);
+                    })
+                } else {
+                    alert("Ошибка аутентефикации");
+                }
+            })  
+    }
 
     return (
         <div className="wrapper">
             <div className="form-box">
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Auth</h1>
                     <div className="input-box"><input type="text" placeholder="Username" name="username" required/><FaUser className='icon'/></div>
                     <div className="input-box"><input type="password" placeholder="Password" name="password" required/><FaLock className='icon'/></div>
-                    <button className='submit' type="submit">Register</button>
+                    <button className='submit' type="submit">Login</button>
                 </form>
                 <div className="register-link">
-                    <p>Don't have an account? <button className="button-link" onClick={handle}>Login</button></p>
+                    <p>Don't have an account? <button className="button-link" onClick={handle}>Registration</button></p>
                 </div>
             </div>
         </div>
--- src/components/login/register/Register.jsx
+++ src/components/login/register/Register.jsx
@@ -1,21 +1,46 @@
 import './Register.css';
-import {FaUser, FaLock} from "react-icons/fa";
+import {FaUser, FaLock, FaEnvelope} from "react-icons/fa";
 
 const Register = ({setAuthPage}) => {
 
-    const handle = (event) => setAuthPage();
+    const handle = () => setAuthPage();
+
+    const delay = (delayInms) => {return new Promise(resolve => setTimeout(resolve, delayInms));};
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+
+        fetch("http://localhost:8080/register", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify({
+                "username": event.target.username.value,
+                "password": event.target.password.value,
+                "email": event.target.email.value
+            })})
+                .then(response => {
+                    if (response.ok) {
+                        alert("Пользователь успешно зарегистрирован");
+                        delay(3000);
+                        handle();
+                    } else {
+                        alert("Пользователь уже существует");
+                    }
+                })
+    }
 
     return (
         <div className="wrapper">
             <div className="form-box">
-                <form>
+                <form onSubmit={handleRegister}>
                     <h1>Register</h1>
                     <div className="input-box"><input type="text" placeholder="Username" name="username" required/><FaUser className='icon'/></div>
                     <div className="input-box"><input type="password" placeholder="Password" name="password" required/><FaLock className='icon'/></div>
+                    <div className="input-box"><input type="email" placeholder="Email" name="email" required/><FaEnvelope className='icon'/></div>
                     <button className='submit' type="submit">Register</button>
                 </form>
                 <div className="register-link">
-                    <p>Don't have an account? <button className="button-link" onClick={handle}>Login</button></p>
+                    <p>Already have an account? <button className="button-link" onClick={handle}>Login</button></p>
                 </div>
             </div>
         </div>
